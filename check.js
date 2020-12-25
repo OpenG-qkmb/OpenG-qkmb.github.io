062cc6addc53cf502316ecfb57f1d5a1464ddbec
OpenG-qkmb <63486452+OpenG-qkmb@users.noreply.github.com>
2020-12-25 12:21:04 +0800

    更新copyright
    更新copyright

--- check.js
+++ check.js
@@ -1,4 +1,4 @@
-var str="哈儿实验室&nbsp;&copy;&nbsp;2018-2020&nbsp;保留所有权利。";
+var str="哈儿实验室&nbsp;&copy;&nbsp;2018-2021&nbsp;保留所有权利。";
 document.getElementById("about").innerHTML=str;
 function test()
 {
