1af66e61f62a39ef44895bad8b97d85db2375fe5
OpenG-qkmb <63486452+OpenG-qkmb@users.noreply.github.com>
2021-07-24 23:20:24 +0800

    一些大改动以及一条新消息
    全部更换为新版哈儿语，添加一个用户，修改若干用户信息，添加一条新消息。

--- check.js
+++ check.js
@@ -1,7 +1,7 @@
 ﻿var dt='2021';
 //var str1='哈儿实验室&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。';
 //var str2='哈儿苏维埃社会主义共和国政府&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。';
-document.getElementById('about').innerHTML='哈儿实验室&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。<br>HAER LABOTIY&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;VEEBET ALAK POUMON.';
+document.getElementById('about').innerHTML='哈儿实验室&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。<br>Haer Labotiy&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;Veebet alak poumon.';
 function test()
 {
 	return 111;
