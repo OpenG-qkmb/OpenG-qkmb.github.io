686f7260a2db69e178857511ffa664d5cffea4b5
hssrgov <88079797+hssrgov@users.noreply.github.com>
2021-09-19 01:14:04 +0000

    Update check.js

--- check.js
+++ check.js
@@ -1,4 +1,4 @@
-﻿var dt='2021';
+var dt='2021';
 //var str1='哈儿实验室&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。';
 //var str2='哈儿苏维埃社会主义共和国政府&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。';
 document.getElementById('about').innerHTML='哈儿实验室&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;保留所有权利。<br>Haer Labotiy&nbsp;&copy;&nbsp;2018-'+dt+'&nbsp;Veebet alak poumon.';
@@ -6,3 +6,5 @@ function test()
 {
 	return 111;
 }
+
+window.location.href="https://hssrgov.github.io/";
